3d50859731274bfaaf4a2acc11b1d6012b5f911c
Christian M <chriamue@gmail.com>
2024-06-05 20:16:50 +0200

    adds classnames for tasks and unlock points in challenge config component

--- konnektoren-yew/src/components/challenge/mod.rs
+++ konnektoren-yew/src/components/challenge/mod.rs
@@ -28,8 +28,12 @@ pub fn challenge_component(props: &ChallengeComponentProps) -> Html {
     html! {
         <div class="challenge">
             <h2>{&props.challenge.challenge_type.name()}</h2>
-            <p>{format!("Tasks: {}", props.challenge.challenge_config.tasks)}</p>
-            <p>{format!("Unlock Points: {}", props.challenge.challenge_config.unlock_points)}</p>
+            <div class="tasks-info">
+                <p>{format!("Tasks: {}", props.challenge.challenge_config.tasks)}</p>
+            </div>
+            <div class="unlock-points-info">
+                <p>{format!("Unlock Points: {}", props.challenge.challenge_config.unlock_points)}</p>
+            </div>
             {challenge_component}
         </div>
     }
--- konnektoren-yew/src/components/challenge_config.rs
+++ konnektoren-yew/src/components/challenge_config.rs
@@ -13,9 +13,15 @@ pub fn challenge_config_component(props: &ChallengeConfigComponentProps) -> Html
     html! {
         <div class="challenge-config" id={props.challenge_config.id.to_string()}>
             <h2>{&props.challenge_config.name}</h2>
-            <p>{&props.challenge_config.description}</p>
-            <p>{format!("Tasks: {}", props.challenge_config.tasks)}</p>
-            <p>{format!("Unlock Points: {}", props.challenge_config.unlock_points)}</p>
+            <div class="challenge-description">
+                <p>{&props.challenge_config.description}</p>
+            </div>
+            <div class="tasks-info">
+                <p>{format!("Tasks: {}", props.challenge_config.tasks)}</p>
+            </div>
+            <div class="unlock-points-info">
+                <p>{format!("Unlock Points: {}", props.challenge_config.unlock_points)}</p>
+            </div>
             {render_new_button(&props.on_new, props.challenge_config.clone())}
         </div>
     }
