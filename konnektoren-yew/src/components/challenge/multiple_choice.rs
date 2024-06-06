6b375ad6ea39df2b91ac6212f63cd97cdaf5664a
Christian M <chriamue@gmail.com>
2024-06-06 10:01:35 +0200

    click on option goes to next

--- konnektoren-yew/src/components/challenge/multiple_choice.rs
+++ konnektoren-yew/src/components/challenge/multiple_choice.rs
@@ -13,49 +13,69 @@ pub struct MultipleChoiceComponentProps {
 #[function_component(MultipleChoiceComponent)]
 pub fn multiple_choice_component(props: &MultipleChoiceComponentProps) -> Html {
     let task_index = use_state(|| 0);
-    let challenge_result = use_state(|| ChallengeResult::default());
-    let with_help = use_state(|| false);
+    let challenge_result = use_state(ChallengeResult::default);
+    let show_help = use_state(|| false);
 
-    let task_count = props.challenge.questions.len();
+    let handle_action = {
+        let task_index = task_index.clone();
+        let show_help = show_help.clone();
+        let total_tasks = props.challenge.questions.len();
 
-    let task_index_clone = task_index.clone();
-    let with_help_clone = with_help.clone();
-    let on_action = Callback::from(move |action| {
-        let task_index = task_index_clone.clone();
-        let with_help = with_help_clone.clone();
-        match action {
+        Callback::from(move |action: ChallengeActions| match action {
             ChallengeActions::Next => {
-                log::info!("Next");
-                task_index.set((*task_index + 1) % task_count);
+                if *task_index < total_tasks - 1 {
+                    task_index.set(*task_index + 1);
+                }
             }
             ChallengeActions::Previous => {
-                log::info!("Previous");
-                task_index.set((*task_index + task_count - 1) % task_count);
+                if *task_index > 0 {
+                    task_index.set(*task_index - 1);
+                }
             }
             ChallengeActions::Help => {
-                log::info!("Help");
-                with_help.set(!*with_help);
+                show_help.set(!*show_help);
             }
-        }
-    });
+        })
+    };
 
-    let challenge_result_clone = challenge_result.clone();
-    let on_option = Callback::from(move |option: MultipleChoiceOption| {
-        log::info!("Option: {}", option.name);
-        let mut challenge_result_update = (*challenge_result_clone).clone();
-        challenge_result_update
-            .add_input(ChallengeInput::MultipleChoice(option.clone()))
-            .unwrap();
-        challenge_result_clone.set(challenge_result_update);
-    });
+    let handle_option_selection = {
+        let task_index = task_index.clone();
+        let challenge_result_clone = challenge_result.clone();
+        let total_tasks = props.challenge.questions.len();
+
+        Callback::from(move |option: MultipleChoiceOption| {
+            let mut challenge_result_update = (*challenge_result_clone).clone();
+            challenge_result_update
+                .add_input(ChallengeInput::MultipleChoice(option.clone()))
+                .unwrap();
+            challenge_result_clone.set(challenge_result_update);
+
+            if *task_index < total_tasks - 1 {
+                task_index.set(*task_index + 1);
+            }
+        })
+    };
 
     html! {
         <div class="multiple-choice">
-            <ProgressBar value={*task_index} max={task_count} label={format!("Question {} of {}", *task_index, task_count)}/>
-            <QuestionComponent question={props.challenge.questions[*task_index].clone()} help={*with_help} />
-            <OptionsComponent options={props.challenge.options.clone()} on_select={on_option} />
-            <ChallengeActionsComponent {on_action} />
-            <MultipleChoiceResultComponent challenge={props.challenge.clone()} challenge_result={(*challenge_result).clone()} />
+            <ProgressBar
+                value={*task_index}
+                max={props.challenge.questions.len()}
+                label={format!("Question {} of {}", *task_index + 1, props.challenge.questions.len())}
+            />
+            <QuestionComponent
+                question={props.challenge.questions[*task_index].clone()}
+                help={*show_help}
+            />
+            <OptionsComponent
+                options={props.challenge.options.clone()}
+                on_select={handle_option_selection}
+            />
+            <ChallengeActionsComponent on_action={handle_action} />
+            <MultipleChoiceResultComponent
+                challenge={props.challenge.clone()}
+                challenge_result={(*challenge_result).clone()}
+            />
         </div>
     }
 }
