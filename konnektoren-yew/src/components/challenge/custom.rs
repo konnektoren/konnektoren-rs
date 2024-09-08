5caf8ac8979dffae807dfee2f88f06e0c5dd95ea
Christian M <chriamue@gmail.com>
2024-09-08 11:01:14 +0200

    refactor custom challenge to use konnektoren js

--- konnektoren-yew/src/components/challenge/custom.rs
+++ konnektoren-yew/src/components/challenge/custom.rs
@@ -1,12 +1,7 @@
 use crate::components::ChallengeEvent;
 use gloo::net::http::Request;
-use gloo::utils::format::JsValueSerdeExt;
-use konnektoren_core::challenges::{ChallengeResult, Custom};
-use std::cell::RefCell;
-use std::rc::Rc;
-use wasm_bindgen::closure::Closure;
-use wasm_bindgen::{JsCast, JsValue};
-use wasm_bindgen_futures::js_sys::eval;
+use konnektoren_core::challenges::{ChallengeResult, Custom, KonnektorenJs};
+use wasm_bindgen::JsValue;
 use yew::prelude::*;
 
 #[derive(Properties, Clone, PartialEq)]
@@ -25,9 +20,7 @@ pub fn custom_component(props: &CustomComponentProps) -> Html {
     let js_content = use_state(|| "".to_string());
     let loading = use_state(|| true);
 
-    // Store the closure inside Rc<RefCell<>> to persist it
-    let js_event_callback =
-        use_state(|| Rc::new(RefCell::new(None::<Closure<dyn FnMut(JsValue)>>)));
+    let konnektoren_js = use_state(|| KonnektorenJs::new());
 
     {
         let html_content = html_content.clone();
@@ -54,46 +47,25 @@ pub fn custom_component(props: &CustomComponentProps) -> Html {
     }
 
     {
-        let js_code = (*js_content).clone();
         let challenge_json = serde_json::to_string_pretty(&props.challenge).unwrap();
-        let event_callback = props.on_event.clone();
+        let js_code = (*js_content).clone();
+        let on_event = props.on_event.clone();
+        let konnektoren_js = konnektoren_js.clone();
 
-        let js_event_callback_clone = js_event_callback.clone();
-        use_effect_with(event_callback.clone(), move |_| {
-            let closure = Closure::wrap(Box::new(move |event: JsValue| {
-                let event_callback = event_callback.clone();
-                if let Some(event_callback) = event_callback.as_ref() {
+        use_effect_with((challenge_json.clone(), js_code.clone()), move |_| {
+            konnektoren_js.set_challenge_data(&challenge_json);
+
+            konnektoren_js.expose_send_event(move |event: JsValue| {
+                if let Some(on_event_callback) = &on_event {
                     let challenge_event: ChallengeEvent = event.into();
-                    event_callback.emit(challenge_event);
+                    on_event_callback.emit(challenge_event);
                 }
-            }) as Box<dyn FnMut(JsValue)>);
+            });
 
-            *js_event_callback_clone.borrow_mut() = Some(closure);
+            let complete_js_code = format!("const challenge = {}; {}", challenge_json, js_code);
+            konnektoren_js.execute_js(&complete_js_code);
 
             || ()
-        });
-
-        let event_callback = props.on_event.clone();
-
-        use_effect_with((js_code.clone(), event_callback.clone()), move |_| {
-            let complete_js_code = format!("const challenge = {}; {}", challenge_json, js_code);
-
-            let window = web_sys::window().unwrap();
-            let global_obj = window.as_ref();
-            let callback_name = JsValue::from_str("emit_challenge_event");
-
-            if let Some(js_event_callback) = &*js_event_callback.borrow() {
-                let callback_ref = js_event_callback.as_ref().unchecked_ref();
-                if let Err(err) = js_sys::Reflect::set(global_obj, &callback_name, callback_ref) {
-                    log::error!("Failed to set callback on window object: {:?}", err);
-                }
-            }
-
-            if let Err(err) = eval(&complete_js_code) {
-                log::error!("JavaScript execution failed: {:?}", err);
-            }
-
-            || () // Clean-up function
         });
     }
 
