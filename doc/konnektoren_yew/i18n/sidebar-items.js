245eaa519fccb4ec90bac0a27bc7392b15e8a4cb
chriamue <chriamue@users.noreply.github.com>
2024-09-24 13:37:04 +0000

    deploy: 1490dc4dbbb2d0eee963fcd46e30e9021eb016ac

--- doc/konnektoren_yew/i18n/sidebar-items.js
+++ doc/konnektoren_yew/i18n/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["LANGUAGES","LANGUAGE_KEY"],"fn":["flag","supported_language","use_i18n","use_selected_language"],"struct":["I18nConfig","I18nProvider","I18nProviderProps","SelectedLanguage"]};
+window.SIDEBAR_ITEMS = {"constant":["LANGUAGES","LANGUAGE_KEY"],"fn":["flag","supported_language","use_i18n","use_selected_language"],"struct":["I18nConfig","I18nJsonLoader","I18nProvider","I18nProviderProps","I18nYmlLoader","SelectedLanguage"],"trait":["I18nLoader"]};
