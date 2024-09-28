30862c96219a021593df8660ca01929593da55e1
chriamue <chriamue@users.noreply.github.com>
2024-09-28 09:21:15 +0000

    deploy: de1b7e2f29744445e32403d602ad792655594ce1

---
+++ doc/trait.impl/konnektoren_core/challenges/challenge_stats/trait.ChallengeStats.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"konnektoren_core":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
