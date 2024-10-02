8836dba7e6bcb6a069938e43c2fd6eab8ca6032f
Christian M <chriamue@gmail.com>
2024-10-02 18:54:29 +0200

    refactors challenge info component

--- konnektoren-yew/src/components/challenge_info.rs
+++ konnektoren-yew/src/components/challenge_info.rs
@@ -16,47 +16,24 @@ pub fn challenge_info(props: &Props) -> Html {
 
     let rating_component = match props.api_url {
         Some(ref api_url) => html! {
-            <ChallengeRatingComponent api_url={api_url.clone()} challenge_id={props.challenge_config.id.clone()} />
+            <div class="challenge-info__rating">
+                <ChallengeRatingComponent api_url={api_url.clone()} challenge_id={props.challenge_config.id.clone()} />
+            </div>
         },
         None => html! {},
     };
 
     html! {
         <div class="challenge-info">
-            <h2>{&props.challenge_config.name}</h2>
+            <h2 class="challenge-info__title">{&props.challenge_config.name}</h2>
             {rating_component}
-            <div class="challenge-description">
+            <div class="challenge-info__description">
                 <p>{&props.challenge_config.description}</p>
             </div>
-            <div class="tasks-info">
-                <p>{format!("{}: {}", i18n.t("Tasks"), props.challenge_config.tasks)}</p>
-            </div>
-            <div class="unlock-points-info">
-                <p>{format!("{}: {}", i18n.t("Unlock Points"), props.challenge_config.unlock_points)}</p>
+            <div class="challenge-info__meta">
+                <p class="challenge-info__tasks">{format!("{}: {}", i18n.t("Tasks"), props.challenge_config.tasks)}</p>
+                <p class="challenge-info__unlock-points">{format!("{}: {}", i18n.t("Unlock Points"), props.challenge_config.unlock_points)}</p>
             </div>
         </div>
     }
-}
-
-#[cfg(feature = "yew-preview")]
-mod preview {
-    use super::*;
-    use yew_preview::prelude::*;
-
-    yew_preview::create_preview!(
-        ChallengeInfoComponent,
-        Props {
-            challenge_config: ChallengeConfig {
-                id: "".to_string(),
-                name: "Challenge Name".to_string(),
-                description: "Challenge Description".to_string(),
-                challenge: "".to_string(),
-                variant: None,
-                tasks: 5,
-                unlock_points: 10,
-                position: None,
-            },
-            api_url: Some("https://api.example.com/reviews".to_string()),
-        },
-    );
 }
