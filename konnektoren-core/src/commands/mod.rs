0cafa308a396eea3c461b17a2e2c9f029cee20b8
Christian M <chriamue@gmail.com>
2024-10-15 19:37:44 +0200

    removes old game commands

--- konnektoren-core/src/commands/mod.rs
+++ konnektoren-core/src/commands/mod.rs
@@ -1,14 +1,9 @@
 //! This module contains the command pattern implementation for the game.
-use anyhow::Result;
-
 pub mod challenge_command;
 pub mod command;
 pub mod game_command;
 
-use crate::game::GameState;
-
-pub mod game_commands;
-
-pub trait GameCommand {
-    fn execute(&self, state: &mut GameState) -> Result<()>;
-}
+pub use game_command::GameCommand;
+pub use challenge_command::ChallengeCommand;
+pub use command::CommandTrait;
+pub use command::Command;
