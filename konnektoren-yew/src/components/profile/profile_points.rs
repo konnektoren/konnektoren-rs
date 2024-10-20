9413b8b54b2f281d189c59b22039731d299166b5
Christian M <chriamue@gmail.com>
2024-10-20 09:58:54 +0200

    use profile hook in profile points

--- konnektoren-yew/src/components/profile/profile_points.rs
+++ konnektoren-yew/src/components/profile/profile_points.rs
@@ -1,27 +1,9 @@
-use crate::providers::use_profile_repository;
-use crate::repository::PROFILE_STORAGE_KEY;
-use konnektoren_core::prelude::PlayerProfile;
+use crate::providers::use_profile;
 use yew::prelude::*;
 
 #[function_component(ProfilePointsComponent)]
 pub fn profile_points_component() -> Html {
-    let profile_repository = use_profile_repository();
-    let profile = use_state(|| PlayerProfile::default());
-
-    {
-        let profile = profile.clone();
-        let profile_repository = profile_repository.clone();
-        use_effect_with((), move |_| {
-            wasm_bindgen_futures::spawn_local(async move {
-                if let Ok(Some(loaded_profile)) =
-                    profile_repository.get_profile(PROFILE_STORAGE_KEY).await
-                {
-                    profile.set(loaded_profile);
-                }
-            });
-            || ()
-        });
-    }
+    let profile = use_profile();
 
     let points = profile.xp;
 
