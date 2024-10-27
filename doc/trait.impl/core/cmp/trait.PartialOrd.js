a231a2b8314b7208cc84a00e748b128b060a2326
chriamue <chriamue@users.noreply.github.com>
2024-10-27 14:28:54 +0000

    deploy: f9dec01014be0f04b0ed48c471b2505d053adce1

---
+++ doc/trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["konnektoren_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"konnektoren_core/challenges/performance_record/struct.PerformanceRecord.html\" title=\"struct konnektoren_core::challenges::performance_record::PerformanceRecord\">PerformanceRecord</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[393]}
