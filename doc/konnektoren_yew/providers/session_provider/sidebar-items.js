4b6647a17b3c729bd14f47c101b2cbfd945d1b0b
chriamue <chriamue@users.noreply.github.com>
2024-10-28 18:46:52 +0000

    deploy: 20c0d4faf0ebbc91d45d25e983a6ae7a4b44d304

---
+++ doc/konnektoren_yew/providers/session_provider/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["SessionContext","SessionProvider","SessionProviderProps"]};
---
+++ doc/konnektoren_yew/providers/settings_provider/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["SettingsContext","SettingsProvider","SettingsProviderProps"]};
