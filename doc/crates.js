13513b5c312d35f8e014ad657362e5240c5c7a6f
chriamue <chriamue@users.noreply.github.com>
2024-12-17 08:03:17 +0000

    deploy: 104f64e15428005ade347b480e318e3816ca7295

--- doc/crates.js
+++ doc/crates.js
@@ -1,2 +1,2 @@
-window.ALL_CRATES = ["konnektoren_core","konnektoren_tests","konnektoren_tui","konnektoren_yew","konnektoren_yew_bin"];
-//{"start":21,"fragment_lengths":[18,20,18,18,22]}
+window.ALL_CRATES = ["konnektoren_core","konnektoren_tests","konnektoren_tui"];
+//{"start":21,"fragment_lengths":[18,20,18]}
--- doc/trait.impl/konnektoren_core/persistence/game_state_persistence/trait.GameStatePersistence.js
+++ doc/trait.impl/konnektoren_core/persistence/game_state_persistence/trait.GameStatePersistence.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["konnektoren_core",[]],["konnektoren_yew",[["impl <a class=\"trait\" href=\"konnektoren_core/persistence/game_state_persistence/trait.GameStatePersistence.html\" title=\"trait konnektoren_core::persistence::game_state_persistence::GameStatePersistence\">GameStatePersistence</a> for <a class=\"struct\" href=\"konnektoren_yew/repository/struct.GameStatePersistenceImpl.html\" title=\"struct konnektoren_yew::repository::GameStatePersistenceImpl\">GameStatePersistenceImpl</a>"]]]]);
+    var implementors = Object.fromEntries([["konnektoren_core",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[23,457]}
+//{"start":57,"fragment_lengths":[23]}
