e71276ab54bb3c72c15c0e9df5280e0e220d6493
chriamue <chriamue@users.noreply.github.com>
2025-01-19 10:09:13 +0000

    deploy: 622e7a484af6be88a159ec73c85cab7ccbe5ad3d

--- doc/konnektoren_core/marketplace/sidebar-items.js
+++ doc/konnektoren_core/marketplace/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CheckoutState"],"struct":["Cart","Coupon","Payment","Product","ProductCatalog"]};
+window.SIDEBAR_ITEMS = {"enum":["CheckoutState","CouponRedemptionError"],"struct":["Cart","Coupon","Payment","Product","ProductCatalog"]};
