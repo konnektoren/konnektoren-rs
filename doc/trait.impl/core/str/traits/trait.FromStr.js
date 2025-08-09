d8f5d39227e59a8daae39ff05fe658d7484df85b
chriamue <chriamue@users.noreply.github.com>
2025-08-09 07:53:37 +0000

    deploy: 5aee4c8d48b68cc41a47d8132d7e778d3a903b90

--- doc/trait.impl/core/str/traits/trait.FromStr.js
+++ doc/trait.impl/core/str/traits/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["konnektoren_platform",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"konnektoren_platform/i18n/language/enum.Language.html\" title=\"enum konnektoren_platform::i18n::language::Language\">Language</a>"]]]]);
+    var implementors = Object.fromEntries([["konnektoren_platform",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"konnektoren_platform/i18n/language/enum.Language.html\" title=\"enum konnektoren_platform::i18n::language::Language\">Language</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
